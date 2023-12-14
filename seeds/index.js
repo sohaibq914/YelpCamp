fd04a0b1a0e12035cd2fe16ea6ee9af7197ef48f
sohaibq914 <sohaibq914@gmail.com>
2023-12-14 17:47:42 -0500

    added basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
   await Campground.deleteMany({}); // delete all
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000); // 1000s cities
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`, // pass the desc and places arrays to sample
+      image: `https://source.unsplash.com/random/300x300?camping,${i}`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ex totam blanditiis illum fugiat perferendis quidem ut debitis voluptate modi, molestiae beatae optio sequi distinctio veniam consectetur et suscipit! Quod.",
+      price, // short for price: price
     });
     await camp.save();
   }
