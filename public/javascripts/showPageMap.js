43a1221eac7a83e034c40bc172d19a4660bf2fb0
sohaibq914 <sohaibq914@gmail.com>
2024-01-06 00:14:48 -0500

    styles and security

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/satellite-v9", // style URL
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 8, // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
--- schemas.js
+++ schemas.js
@@ -1,15 +1,39 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
 
 // validations on the server side (can't bypass even thru postman)
 // not a mongoose schema... this will validate before saving it to mongoose
 
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
+
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required(), // title is required
+    title: Joi.string().required().escapeHTML(), // title is required
     price: Joi.number().required().min(0), // price is required and min is 0
     // image: Joi.string().required(),
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
   }).required(), // saying the campground is required
   deleteImages: Joi.array(),
 });
@@ -17,6 +41,6 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
   }).required(),
 });
